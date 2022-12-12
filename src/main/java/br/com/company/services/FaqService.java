1f49b0a4bd8683dc5d818f06e7d0c1ae533e7d5d
Israel.Sestrem <israel.sestrem@senior.com.br>
2022-12-12 15:56:02 -0300

    faq service

--- src/main/java/br/com/company/services/FaqService.java
+++ src/main/java/br/com/company/services/FaqService.java
@@ -1,8 +1,13 @@
 package br.com.company.services;
 
+import br.com.company.entities.FaqEntity;
 import br.com.company.repositories.FaqRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class FaqService {
@@ -10,6 +15,29 @@ public class FaqService {
     @Autowired
     FaqRepository faqRepository;
 
+    @Transactional(readOnly = true)
+    public List<FaqEntity> findAll(){
+        return faqRepository.findAll();
+    }
 
+    @Transactional(readOnly = true)
+    public Optional<FaqEntity> findById(Integer id){
+        return faqRepository.findById(id);
+    }
+
+    @Transactional
+    public FaqEntity save(FaqEntity faq){
+        return faqRepository.save(faq);
+    }
+
+    @Transactional(readOnly = true)
+    public boolean existsById(Integer id){
+        return faqRepository.existsById(id);
+    }
+
+    @Transactional
+    public void deleteById(Integer id){
+        faqRepository.deleteById(id);
+    }
 
 }
