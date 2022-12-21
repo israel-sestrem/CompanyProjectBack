871ee4dabd8bd3d95ae835eeab47328b19be8be9
Israel.Sestrem <israel.sestrem@senior.com.br>
2022-12-21 13:20:42 -0300

    classes de base para testes

---
+++ src/test/java/br/com/company/services/AddressServiceTest.java
@@ -0,0 +1,8 @@
+package br.com.company.services;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AddressServiceTest {
+}
